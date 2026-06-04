bc434da975200237ba83ba727529d620af1f1335
Tawan <chawinthorn.kit@student.mahidol.edu>
2026-06-05 03:29:59 +0700

    Issue #27; feat: integrate working auth flow.

--- codes/frontend/restaurant/src/home/StaffHome.tsx
+++ codes/frontend/restaurant/src/home/StaffHome.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import { LogoutButton } from '../authentication/LogoutButton';
 
 export default function Home() {
     return (
-        <div>
-            <h1>Home</h1>
-        </div>
-    );
+            <div>
+                <h1>Home</h1>
+                <div style={{ marginTop: '20px' }}>
+                    <LogoutButton />
+                </div>
+            </div>
+        );
 }
