3fc889f692984573a40235114f0f03054d5a06b1
Tawan <chawinthorn.kit@student.mahidol.edu>
2026-06-12 22:05:19 +0700

    Issue #None; chore: update paths after file relocation

--- codes/backend/restaurant/src/main/kotlin/com/food/restaurant/repository/UserRepository.kt
+++ codes/backend/restaurant/src/main/kotlin/com/food/restaurant/repository/UserRepository.kt
@@ -1,6 +1,6 @@
 package com.food.restaurant.repository
 
-import com.food.restaurant.entity.User
+import com.food.restaurant.entity.user.User
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 import java.util.UUID
