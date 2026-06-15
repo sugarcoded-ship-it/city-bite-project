cc2eb7f217866425f7293764c673e69b148c4b8d
6681399 <Nattapas.nun@student.mahidol.edu>
2026-06-15 21:22:52 +0700

    Issue #none; fix: remove picture column in database and change time to date for leaveDay table

--- codes/backend/restaurant/src/main/kotlin/com/food/restaurant/entity/user/LeaveDay.kt
+++ codes/backend/restaurant/src/main/kotlin/com/food/restaurant/entity/user/LeaveDay.kt
@@ -9,12 +9,12 @@ import jakarta.persistence.Id
 import jakarta.persistence.JoinColumn
 import jakarta.persistence.ManyToOne
 import jakarta.persistence.Table
+import java.sql.Timestamp
 import java.time.LocalDate
-import java.time.LocalTime
 
 @Entity
-@Table(name = "Staff_Schedule")
-class StaffSchedule(
+@Table(name = "Leave_Day")
+class LeaveDay(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "Schedule_ID")
@@ -24,14 +24,14 @@ class StaffSchedule(
     @JoinColumn(name = "keycloak_uuid", nullable = false)
     var staff: User,
 
-    @Column(name = "Date", nullable = false)
-    var date: LocalDate,
+    @Column(name = "startDate", nullable = false)
+    var startDate: LocalDate,
 
-    @Column(name = "start_time", nullable = false)
-    var startTime: LocalTime,
+    @Column(name = "endDate", nullable = false)
+    var endDate: LocalDate,
 
-    @Column(name = "end_time", nullable = false)
-    var endTime: LocalTime,
+    @Column(name = "createdAt", nullable = false)
+    var date: Timestamp,
 
     @Column(name = "status", nullable = false)
     var status: String
