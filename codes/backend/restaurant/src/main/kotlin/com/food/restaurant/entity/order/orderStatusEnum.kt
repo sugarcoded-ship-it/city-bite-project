0b7ecb39f438ec97dbfaf44f0cb8b8bfc93942e8
6681399 <Nattapas.nun@student.mahidol.edu>
2026-06-17 21:11:36 +0700

    Issue #15; feat: create Enum for every tables that are needed

---
+++ codes/backend/restaurant/src/main/kotlin/com/food/restaurant/entity/order/orderStatusEnum.kt
@@ -0,0 +1,21 @@
+package com.food.restaurant.entity.order
+
+import com.fasterxml.jackson.annotation.JsonValue
+
+enum class orderStatusEnum(val displayName: String) {
+    PENDING("Pending"),
+    RECEIVED("Received"),
+    PROCESSING("Processing"),
+    ON_DELIVERY("On Delivery"),
+    SHIPPED("Shipped");
+
+    // This makes Json sends the displayName in API instead of the actual enum. It looks more beautiful
+    @JsonValue
+    fun toValue(): String = displayName
+
+    companion object {
+        fun fromDisplayName(name: String): orderStatusEnum? {
+            return entries.find { it.displayName.equals(name, ignoreCase = true) }
+        }
+    }
+}
