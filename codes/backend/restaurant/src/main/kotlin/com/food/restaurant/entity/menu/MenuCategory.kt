c1180e09e2c127d95080c37ca834acda61e9ebed
u6680972 <natthakul.yis@student.mahidol.edu>
2026-06-18 13:37:11 +0700

    fix: map MenuStatus/MenuCategory enums via displayName instead of enum name

--- codes/backend/restaurant/src/main/kotlin/com/food/restaurant/entity/menu/MenuCategory.kt
+++ codes/backend/restaurant/src/main/kotlin/com/food/restaurant/entity/menu/MenuCategory.kt
@@ -2,8 +2,6 @@ package com.food.restaurant.entity.menu
 
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
-import jakarta.persistence.EnumType
-import jakarta.persistence.Enumerated
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
@@ -17,7 +15,6 @@ class MenuCategory(
     @Column(name = "Category_ID")
     var id: Int = 0,
 
-    @Enumerated(EnumType.STRING)
     @Column(name = "name", nullable = false)
     var name: menuCategoryEnum
 )
