3af16cca7b45e437de5a910de7a0013aeb1674b3
u6680972 <natthakul.yis@student.mahidol.edu>
2026-06-18 12:40:33 +0700

    feat: create MenuRepository

---
+++ codes/backend/restaurant/src/main/kotlin/com/food/restaurant/repository/MenuRepository.kt
@@ -0,0 +1,13 @@
+package com.food.restaurant.repository
+
+import com.food.restaurant.entity.menu.MenuItem
+import com.food.restaurant.entity.menu.menuCategoryEnum
+import com.food.restaurant.entity.menu.menuStatusEnum
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface MenuRepository : JpaRepository<MenuItem, Int> {
+    fun findByStatus_Name(statusName: menuStatusEnum): List<MenuItem>
+    fun findByStatus_NameAndCategory_Name(statusName: menuStatusEnum, categoryName: menuCategoryEnum): List<MenuItem>
+}
