bd1fcd771fb5055b661fe490af3ffa2abca601cc
u6680972 <natthakul.yis@student.mahidol.edu>
2026-06-19 11:29:03 +0700

    Issue#3; fix: apiClient can pass every method

--- codes/frontend/restaurant/src/api/client.ts
+++ codes/frontend/restaurant/src/api/client.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import type { InternalAxiosRequestConfig } from 'axios';
+import type { AxiosRequestConfig } from 'axios';
 import keycloak from '../security/keycloak.ts';
 
 export const api = axios.create({
@@ -37,10 +37,9 @@ api.interceptors.response.use(
     }
 );
 
-// Allow whatever pass into the function to decide how the request behaves
 export const apiClient = async <T>(
     endpoint: string,
-    options: Partial<InternalAxiosRequestConfig> = {}
+    options: AxiosRequestConfig<unknown> = {}
 ): Promise<T> => {
     const response = await api({
         url: endpoint,
