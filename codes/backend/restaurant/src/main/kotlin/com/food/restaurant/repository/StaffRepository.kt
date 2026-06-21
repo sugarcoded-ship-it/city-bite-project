7fbba090eb81e9af6a4a2d08b269adf00c3f4c17
u6680972 <natthakul.yis@student.mahidol.edu>
2026-06-21 16:42:55 +0700

    Issue #50; feat: create new repositories to prepare for starting owner dashboard

--- codes/backend/restaurant/src/main/kotlin/com/food/restaurant/repository/StaffRepository.kt
+++ codes/backend/restaurant/src/main/kotlin/com/food/restaurant/repository/StaffRepository.kt
@@ -10,6 +10,7 @@ import java.util.UUID
 
 @Repository
 interface StaffRepository : JpaRepository<Staff, UUID> {
+    fun countByStatus(status: String): Long
     @Query("""
         SELECT new com.food.restaurant.dto.staff.StaffListResponse(
             s.id,
