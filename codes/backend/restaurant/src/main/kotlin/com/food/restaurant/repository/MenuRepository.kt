cca0c8d2398cdc61d6c69bac9e1fa7b50017d830
u6680972 <natthakul.yis@student.mahidol.edu>
2026-06-21 20:40:55 +0700

    Issue #50; fix: change to countByStatus_Name with handles input as Enum

--- codes/backend/restaurant/src/main/kotlin/com/food/restaurant/repository/MenuRepository.kt
+++ codes/backend/restaurant/src/main/kotlin/com/food/restaurant/repository/MenuRepository.kt
@@ -4,7 +4,6 @@ import com.food.restaurant.entity.menu.MenuItem
 import com.food.restaurant.entity.menu.menuCategoryEnum
 import com.food.restaurant.entity.menu.menuStatusEnum
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 
 @Repository
@@ -12,6 +11,5 @@ interface MenuRepository : JpaRepository<MenuItem, Int> {
     fun findByStatus_Name(statusName: menuStatusEnum): List<MenuItem>
     fun findByStatus_NameAndCategory_Name(statusName: menuStatusEnum, categoryName: menuCategoryEnum): List<MenuItem>
 
-    @Query("SELECT COUNT(m) FROM MenuItem m JOIN m.status s WHERE s.name = 'Available'")
-    fun countAvailableItems(): Long
+    fun countByStatus_Name(statusName: menuStatusEnum): Long
 }
