1b4dcbc51d395658ea0b0f5cf6fa0c129a67ac85
u6680972 <natthakul.yis@student.mahidol.edu>
2026-06-21 19:13:16 +0700

    Issue #50; fix: fix cases

--- codes/backend/restaurant/src/main/kotlin/com/food/restaurant/entity/user/LeaveDay.kt
+++ codes/backend/restaurant/src/main/kotlin/com/food/restaurant/entity/user/LeaveDay.kt
@@ -13,24 +13,24 @@ import java.sql.Timestamp
 import java.time.LocalDate
 
 @Entity
-@Table(name = "Leave_Day")
+@Table(name = "leave_day")
 class LeaveDay(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "Leave_Day_ID")
+    @Column(name = "leave_day_id")
     var id: Int = 0,
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "keycloak_uuid", nullable = false)
     var staff: User,
 
-    @Column(name = "startDate", nullable = false)
+    @Column(name = "start_date", nullable = false) // snake_case
     var startDate: LocalDate,
 
-    @Column(name = "endDate", nullable = false)
+    @Column(name = "end_date", nullable = false) // snake_case
     var endDate: LocalDate,
 
-    @Column(name = "createdAt", nullable = false)
+    @Column(name = "created_at", nullable = false) // snake_case
     var date: Timestamp,
 
     @Column(name = "status", nullable = false)
