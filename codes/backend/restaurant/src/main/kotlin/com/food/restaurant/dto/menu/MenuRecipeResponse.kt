6a9226a7d4c7722160b44dcdc236f7c3718d247f
u6680972 <natthakul.yis@student.mahidol.edu>
2026-06-22 17:21:07 +0700

    Issue #50; feat: create MenuRecipeRepository, MenuRecipeRequest, MenuRecipeResponse

---
+++ codes/backend/restaurant/src/main/kotlin/com/food/restaurant/dto/menu/MenuRecipeResponse.kt
@@ -0,0 +1,21 @@
+package com.food.restaurant.dto.menu
+
+import com.food.restaurant.entity.MeasurementUnits
+import com.food.restaurant.entity.menu.MenuRecipe
+import java.math.BigDecimal
+
+data class MenuRecipeResponse(
+    val stockId: Int,
+    val stockName: String,
+    val amount: BigDecimal,
+    val measureUnit: MeasurementUnits
+) {
+    companion object {
+        fun from(menuRecipe: MenuRecipe) = MenuRecipeResponse(
+            stockId = menuRecipe.stock.id,
+            stockName = menuRecipe.stock.name,
+            amount = menuRecipe.amount,
+            measureUnit = menuRecipe.measureUnit
+        )
+    }
+}
