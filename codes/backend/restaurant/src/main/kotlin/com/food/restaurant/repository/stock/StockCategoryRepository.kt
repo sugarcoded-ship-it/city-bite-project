46a789e97ddf760a2a2c674239f9925f792fd857
Pakawun <pakawun.jid@student.mahidol.edu>
2026-07-03 00:14:13 +0700

    Issue #44; fix: missed git commit from local
    Suspected that git didn't track this file on lastest commit, so this address failed CI build test

--- codes/backend/restaurant/src/main/kotlin/com/food/restaurant/repository/stock/StockCategoryRepository.kt
+++ codes/backend/restaurant/src/main/kotlin/com/food/restaurant/repository/stock/StockCategoryRepository.kt
@@ -1,11 +1,8 @@
 package com.food.restaurant.repository.stock
 
 import com.food.restaurant.entity.stock.StockCategory
-import com.food.restaurant.entity.stock.stockCategoryEnum
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface StockCategoryRepository : JpaRepository<StockCategory, Int> {
-    fun findByName(name: stockCategoryEnum): StockCategory?
-}
+interface StockCategoryRepository : JpaRepository<StockCategory, Int>
